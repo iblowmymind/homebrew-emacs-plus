2b1a8e3ed70b10735c7a1b2f1a1024c6288ca16c
iblowmymind <28228415+iblowmymind@users.noreply.github.com>
2025-04-20 21:20:51 +0300

    Readd patch

--- Formula/emacs-plus@31.rb
+++ Formula/emacs-plus@31.rb
@@ -93,6 +93,7 @@ class EmacsPlusAT31 < EmacsBase
   local_patch "fix-window-role", sha: "1f8423ea7e6e66c9ac6dd8e37b119972daa1264de00172a24a79a710efcb8130"
   local_patch "system-appearance", sha: "53283503db5ed2887e9d733baaaf80f2c810e668e782e988bda5855a0b1ebeb4"
   local_patch "round-undecorated-frame", sha: "26947b6724fc29fadd44889808c5cf0b4ce6278cf04f46086a21df50c8c4151d"
+  local_patch "xwidget-user-agent", sha: "c0881328b64bf10f9252ff31837188dc596d2df38c913e0e0f49ec0da30186c7" if build.with? "xwidgets"
 
   #
   # Install
